ccc455e807044677303eb22c968b5e0288c2b960
Mustafa Alsayafi <mustafaalsayafi@gmail.com>
2024-03-09 22:27:16 +0300

    chore: remove old files

--- packages/eslint-config-js/index.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line unicorn/prefer-module
-module.exports = {
-  extends: ['airbnb-base', '@jeniex/eslint-config-base'],
-  // extends: ['airbnb-base', '../eslint-config-base/index.js'],
-};
